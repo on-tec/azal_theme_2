zendesk/copenhagen_theme

feat: add search to Services List

src/modules/service-catalog/ServiceCatalogList.tsx
@@ -1,13 +1,15 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import ServiceCatalogListItem from "./components/service-catalog-list-item/ServiceCatalogListItem";
 import type { ServiceCatalogItem } from "./data-types/ServiceCatalogItem";
 import { Col, Grid, Row } from "@zendeskgarden/react-grid";
 import styled from "styled-components";
 import { useTranslation } from "react-i18next";
-
 import { CursorPagination } from "@zendeskgarden/react-pagination";
 import { LoadingState } from "./components/service-catalog-list-item/LoadingState";
 import { EmptyState } from "./components/service-catalog-list-item/EmptyState";
+import { Search } from "./components/service-catalog-list-item/Search";
+import debounce from "lodash.debounce";
+import { useServiceCatalogItems } from "./useServiceCatalogItems";
 import { useNotify } from "../shared/notifications/useNotify";
 
 const StyledCol = styled(Col)`
@@ -25,83 +27,74 @@ const StyledGrid = styled(Grid)`
   padding: 0;
 `;
 
-const PAGE_SIZE = 16;
-
-type Meta = {
-  before_cursor: string;
-  after_cursor: string;
-  has_more: boolean;
-};
-
 export function ServiceCatalogList({
   helpCenterPath,
 }: {
   helpCenterPath: string;
 }) {
-  const [serviceCatalogItems, setServiceCatalogItems] = useState<
-    ServiceCatalogItem[]
-  >([]);
-  const [meta, setMeta] = useState<Meta | null>(null);
-  const [currentCursor, setCurrentCursor] = useState<string | null>(null);
-  const [count, setCount] = useState(0);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [error, setError] = useState<unknown>(null);
-  const notify = useNotify();
-  const { t } = useTranslation();
+  const [searchInputValue, setSearchInputValue] = useState("");
 
-  if (error) {
-    throw error;
+  const { t } = useTranslation();
+  const notify = useNotify();
+
+  const {
+    serviceCatalogItems,
+    meta,
+    count,
+    isLoading,
+    errorFetchingItems,
+    fetchServiceCatalogItems,
+  } = useServiceCatalogItems();
+
+  if (errorFetchingItems) {
+    notify({
+      title: t(
+        "service-catalog.service-list-error-title",
+        "Services couldn't be loaded"
+      ),
+      message: t(
+        "service-catalog.service-list-error-message",
+        "Give it a moment and try it again"
+      ),
+      type: "error",
+    });
+    throw errorFetchingItems;
   }
 
+  const debouncedUpdateServiceCatalogItems = useMemo(
+    () => debounce(fetchServiceCatalogItems, 300),
+    [fetchServiceCatalogItems]
+  );
+
   useEffect(() => {
-    async function fetchData() {
-      setIsLoading(true);
-      try {
-        const response = await fetch(
-          currentCursor
-            ? `/api/v2/help_center/service_catalog/items?page[size]=${PAGE_SIZE}&${currentCursor}`
-            : `/api/v2/help_center/service_catalog/items?page[size]=${PAGE_SIZE}`
-        );
-        const data = await response.json();
-        if (response.ok) {
-          setMeta(data.meta);
-          setServiceCatalogItems(data.service_catalog_items);
-          setCount(data.count);
-          setIsLoading(false);
-        }
-        if (!response.ok) {
-          setIsLoading(false);
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-      } catch (error) {
-        setIsLoading(false);
-        notify({
-          title: t(
-            "service-catalog.service-list-error-title",
-            "Services couldn't be loaded"
-          ),
-          message: t(
-            "service-catalog.service-list-error-message",
-            "Give it a moment and try it again"
-          ),
-          type: "error",
-        });
-        setError(error);
-      }
-    }
-    fetchData();
-  }, [currentCursor, notify, t]);
+    fetchServiceCatalogItems("", null);
+  }, [fetchServiceCatalogItems]);
+
+  useEffect(() => {
+    return () => debouncedUpdateServiceCatalogItems.cancel();
+  }, [debouncedUpdateServiceCatalogItems]);
 
   const handleNextClick = () => {
     if (meta && meta.after_cursor) {
-      setCurrentCursor(`page[after]=${meta.after_cursor}`);
+      fetchServiceCatalogItems(
+        searchInputValue,
+        "page[after]=" + meta.after_cursor
+      );
     }
   };
 
   const handlePreviousClick = () => {
     if (meta && meta.before_cursor) {
-      setCurrentCursor(`page[before]=${meta?.before_cursor}`);
+      fetchServiceCatalogItems(
+        searchInputValue,
+        "page[before]=" + meta.before_cursor
+      );
     }
+  };
+
+  const handleInputChange = (value: string) => {
+    setSearchInputValue(value);
+    debouncedUpdateServiceCatalogItems(value, null);
   };
 
   return (
@@ -113,6 +106,11 @@ export function ServiceCatalogList({
           count: count,
         })}
       </span>
+      <Search
+        searchInputValue={searchInputValue}
+        isLoading={isLoading}
+        setSearchInputValue={handleInputChange}
+      />
       {isLoading ? (
         <LoadingState />
       ) : (
@@ -132,26 +130,22 @@ export function ServiceCatalogList({
             </Row>
           </StyledGrid>
           {serviceCatalogItems.length === 0 && (
-            <EmptyState helpCenterPath={helpCenterPath} />
+            <EmptyState
+              helpCenterPath={helpCenterPath}
+              searchInputValue={searchInputValue}
+            />
           )}
           {serviceCatalogItems.length > 0 && (
             <CursorPagination>
               <CursorPagination.Previous
                 onClick={handlePreviousClick}
-                disabled={
-                  !currentCursor ||
-                  (currentCursor?.startsWith("page[before]") && !meta?.has_more)
-                }
+                disabled={meta?.before_cursor == null}
               >
                 {t("service-catalog.pagination.previous", "Previous")}
               </CursorPagination.Previous>
               <CursorPagination.Next
                 onClick={handleNextClick}
-                disabled={
-                  (currentCursor?.startsWith("page[after]") &&
-                    !meta?.has_more) ||
-                  (currentCursor == null && !meta?.has_more)
-                }
+                disabled={meta?.after_cursor == null}
               >
                 {t("service-catalog.pagination.next", "Next")}
               </CursorPagination.Next>

src/modules/service-catalog/components/service-catalog-list-item/Search.tsx
@@ -1,0 +1,48 @@
+import { Field, Label, MediaInput } from "@zendeskgarden/react-forms";
+import { Dots } from "@zendeskgarden/react-loaders";
+import SearchIcon from "@zendeskgarden/svg-icons/src/16/search-stroke.svg";
+import { useTranslation } from "react-i18next";
+import styled from "styled-components";
+
+const StyledField = styled(Field)`
+  align-items: center;
+  width: 320px;
+  @media (max-width: 768px) {
+    width: 100%;
+    display: flex;
+  }
+`;
+
+interface SearchProps {
+  searchInputValue: string;
+  isLoading: boolean;
+  setSearchInputValue: (value: string) => void;
+}
+
+export const Search = ({
+  searchInputValue,
+  isLoading,
+  setSearchInputValue,
+}: SearchProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <StyledField>
+      <Label hidden>
+        {t("service-catalog.search-services", "Search for services")}
+      </Label>
+      <MediaInput
+        start={<SearchIcon />}
+        type="search"
+        autoComplete="off"
+        end={isLoading && searchInputValue ? <Dots /> : undefined}
+        value={searchInputValue}
+        placeholder={t(
+          "service-catalog.search-services",
+          "Search for services"
+        )}
+        onChange={(event) => setSearchInputValue(event.target.value)}
+      />
+    </StyledField>
+  );
+};

src/modules/service-catalog/useServiceCatalogItems.spec.ts
@@ -1,0 +1,130 @@
+import { renderHook, act } from "@testing-library/react-hooks";
+import { useServiceCatalogItems } from "./useServiceCatalogItems";
+
+describe("useServiceCatalogItems", () => {
+  const mockResponse = {
+    service_catalog_items: [
+      { id: 1, name: "Item 1", description: "Description 1", form_id: 1 },
+      { id: 2, name: "Item 2", description: "Description 2", form_id: 2 },
+    ],
+    meta: { before_cursor: null, after_cursor: null, has_more: false },
+    count: 2,
+  };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should fetch service catalog items on mount", async () => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(mockResponse),
+        status: 200,
+        ok: true,
+      })
+    ) as jest.Mock;
+
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useServiceCatalogItems()
+    );
+
+    expect(result.current.serviceCatalogItems).toEqual([]);
+    expect(result.current.isLoading).toBe(true);
+
+    await waitForNextUpdate();
+
+    expect(result.current.serviceCatalogItems).toEqual(
+      mockResponse.service_catalog_items
+    );
+    expect(result.current.isLoading).toBe(false);
+    expect(result.current.meta).toEqual(mockResponse.meta);
+    expect(result.current.count).toEqual(mockResponse.count);
+  });
+
+  it("should handle fetch error", async () => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve({ message: "Error fetching" }),
+        status: 500,
+        ok: false,
+      })
+    ) as jest.Mock;
+
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useServiceCatalogItems()
+    );
+
+    expect(result.current.serviceCatalogItems).toEqual([]);
+    expect(result.current.isLoading).toBe(true);
+
+    await waitForNextUpdate();
+
+    expect(result.current.serviceCatalogItems).toEqual([]);
+    expect(result.current.isLoading).toBe(false);
+    expect(result.current.errorFetchingItems).toEqual(
+      new Error("HTTP error! status: 500")
+    );
+  });
+
+  it("should refetch items when fetchServiceCatalogItems is called", async () => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(mockResponse),
+        status: 200,
+        ok: true,
+      })
+    ) as jest.Mock;
+
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useServiceCatalogItems()
+    );
+
+    expect(result.current.serviceCatalogItems).toEqual([]);
+    expect(result.current.isLoading).toBe(true);
+
+    await waitForNextUpdate();
+
+    expect(result.current.serviceCatalogItems).toEqual(
+      mockResponse.service_catalog_items
+    );
+    expect(result.current.isLoading).toBe(false);
+    expect(result.current.meta).toEqual(mockResponse.meta);
+    expect(result.current.count).toEqual(mockResponse.count);
+
+    const newMockResponse = {
+      service_catalog_items: [
+        { id: 3, name: "Item 3", description: "Description 3", form_id: 3 },
+      ],
+      meta: { before_cursor: null, after_cursor: null, has_more: false },
+      count: 3,
+    };
+
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(newMockResponse),
+        status: 200,
+        ok: true,
+      })
+    ) as jest.Mock;
+
+    act(() => {
+      result.current.fetchServiceCatalogItems("Item 3", null);
+    });
+
+    expect(result.current.serviceCatalogItems).toEqual(
+      mockResponse.service_catalog_items
+    );
+    expect(result.current.isLoading).toBe(true);
+
+    await waitForNextUpdate();
+
+    expect(result.current.serviceCatalogItems).toEqual(
+      newMockResponse.service_catalog_items
+    );
+
+    expect(result.current.isLoading).toBe(false);
+
+    expect(result.current.meta).toEqual(newMockResponse.meta);
+    expect(result.current.count).toEqual(newMockResponse.count);
+  });
+});

on-tec/azal_theme_2

src/modules/service-catalog/ServiceCatalogList.tsx
@@ -1,13 +1,15 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import ServiceCatalogListItem from "./components/service-catalog-list-item/ServiceCatalogListItem";
 import type { ServiceCatalogItem } from "./data-types/ServiceCatalogItem";
 import { Col, Grid, Row } from "@zendeskgarden/react-grid";
 import styled from "styled-components";
 import { useTranslation } from "react-i18next";
-
 import { CursorPagination } from "@zendeskgarden/react-pagination";
 import { LoadingState } from "./components/service-catalog-list-item/LoadingState";
 import { EmptyState } from "./components/service-catalog-list-item/EmptyState";
+import { Search } from "./components/service-catalog-list-item/Search";
+import debounce from "lodash.debounce";
+import { useServiceCatalogItems } from "./useServiceCatalogItems";
 import { useNotify } from "../shared/notifications/useNotify";
 
 const StyledCol = styled(Col)`
@@ -25,83 +27,74 @@ const StyledGrid = styled(Grid)`
   padding: 0;
 `;
 
-const PAGE_SIZE = 16;
-
-type Meta = {
-  before_cursor: string;
-  after_cursor: string;
-  has_more: boolean;
-};
-
 export function ServiceCatalogList({
   helpCenterPath,
 }: {
   helpCenterPath: string;
 }) {
-  const [serviceCatalogItems, setServiceCatalogItems] = useState<
-    ServiceCatalogItem[]
-  >([]);
-  const [meta, setMeta] = useState<Meta | null>(null);
-  const [currentCursor, setCurrentCursor] = useState<string | null>(null);
-  const [count, setCount] = useState(0);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [error, setError] = useState<unknown>(null);
-  const notify = useNotify();
-  const { t } = useTranslation();
+  const [searchInputValue, setSearchInputValue] = useState("");
 
-  if (error) {
-    throw error;
+  const { t } = useTranslation();
+  const notify = useNotify();
+
+  const {
+    serviceCatalogItems,
+    meta,
+    count,
+    isLoading,
+    errorFetchingItems,
+    fetchServiceCatalogItems,
+  } = useServiceCatalogItems();
+
+  if (errorFetchingItems) {
+    notify({
+      title: t(
+        "service-catalog.service-list-error-title",
+        "Services couldn't be loaded"
+      ),
+      message: t(
+        "service-catalog.service-list-error-message",
+        "Give it a moment and try it again"
+      ),
+      type: "error",
+    });
+    throw errorFetchingItems;
   }
 
+  const debouncedUpdateServiceCatalogItems = useMemo(
+    () => debounce(fetchServiceCatalogItems, 300),
+    [fetchServiceCatalogItems]
+  );
+
   useEffect(() => {
-    async function fetchData() {
-      setIsLoading(true);
-      try {
-        const response = await fetch(
-          currentCursor
-            ? `/api/v2/help_center/service_catalog/items?page[size]=${PAGE_SIZE}&${currentCursor}`
-            : `/api/v2/help_center/service_catalog/items?page[size]=${PAGE_SIZE}`
-        );
-        const data = await response.json();
-        if (response.ok) {
-          setMeta(data.meta);
-          setServiceCatalogItems(data.service_catalog_items);
-          setCount(data.count);
-          setIsLoading(false);
-        }
-        if (!response.ok) {
-          setIsLoading(false);
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-      } catch (error) {
-        setIsLoading(false);
-        notify({
-          title: t(
-            "service-catalog.service-list-error-title",
-            "Services couldn't be loaded"
-          ),
-          message: t(
-            "service-catalog.service-list-error-message",
-            "Give it a moment and try it again"
-          ),
-          type: "error",
-        });
-        setError(error);
-      }
-    }
-    fetchData();
-  }, [currentCursor, notify, t]);
+    fetchServiceCatalogItems("", null);
+  }, [fetchServiceCatalogItems]);
+
+  useEffect(() => {
+    return () => debouncedUpdateServiceCatalogItems.cancel();
+  }, [debouncedUpdateServiceCatalogItems]);
 
   const handleNextClick = () => {
     if (meta && meta.after_cursor) {
-      setCurrentCursor(`page[after]=${meta.after_cursor}`);
+      fetchServiceCatalogItems(
+        searchInputValue,
+        "page[after]=" + meta.after_cursor
+      );
     }
   };
 
   const handlePreviousClick = () => {
     if (meta && meta.before_cursor) {
-      setCurrentCursor(`page[before]=${meta?.before_cursor}`);
+      fetchServiceCatalogItems(
+        searchInputValue,
+        "page[before]=" + meta.before_cursor
+      );
     }
+  };
+
+  const handleInputChange = (value: string) => {
+    setSearchInputValue(value);
+    debouncedUpdateServiceCatalogItems(value, null);
   };
 
   return (
@@ -113,6 +106,11 @@ export function ServiceCatalogList({
           count: count,
         })}
       </span>
+      <Search
+        searchInputValue={searchInputValue}
+        isLoading={isLoading}
+        onChange={handleInputChange}
+      />
       {isLoading ? (
         <LoadingState />
       ) : (
@@ -132,26 +130,22 @@ export function ServiceCatalogList({
             </Row>
           </StyledGrid>
           {serviceCatalogItems.length === 0 && (
-            <EmptyState helpCenterPath={helpCenterPath} />
+            <EmptyState
+              helpCenterPath={helpCenterPath}
+              searchInputValue={searchInputValue}
+            />
           )}
           {serviceCatalogItems.length > 0 && (
             <CursorPagination>
               <CursorPagination.Previous
                 onClick={handlePreviousClick}
-                disabled={
-                  !currentCursor ||
-                  (currentCursor?.startsWith("page[before]") && !meta?.has_more)
-                }
+                disabled={meta?.before_cursor == null}
               >
                 {t("service-catalog.pagination.previous", "Previous")}
               </CursorPagination.Previous>
               <CursorPagination.Next
                 onClick={handleNextClick}
-                disabled={
-                  (currentCursor?.startsWith("page[after]") &&
-                    !meta?.has_more) ||
-                  (currentCursor == null && !meta?.has_more)
-                }
+                disabled={meta?.after_cursor == null}
               >
                 {t("service-catalog.pagination.next", "Next")}
               </CursorPagination.Next>

src/modules/service-catalog/components/service-catalog-list-item/Search.tsx
@@ -1,0 +1,48 @@
+import { Field, Label, MediaInput } from "@zendeskgarden/react-forms";
+import { Dots } from "@zendeskgarden/react-loaders";
+import SearchIcon from "@zendeskgarden/svg-icons/src/16/search-stroke.svg";
+import { useTranslation } from "react-i18next";
+import styled from "styled-components";
+
+const StyledField = styled(Field)`
+  align-items: center;
+  width: 320px;
+  @media (max-width: 768px) {
+    width: 100%;
+    display: flex;
+  }
+`;
+
+interface SearchProps {
+  searchInputValue: string;
+  isLoading: boolean;
+  onChange: (value: string) => void;
+}
+
+export const Search = ({
+  searchInputValue,
+  isLoading,
+  onChange,
+}: SearchProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <StyledField>
+      <Label hidden>
+        {t("service-catalog.search-services", "Search for services")}
+      </Label>
+      <MediaInput
+        start={<SearchIcon />}
+        type="search"
+        autoComplete="off"
+        end={isLoading && searchInputValue ? <Dots /> : undefined}
+        value={searchInputValue}
+        placeholder={t(
+          "service-catalog.search-services",
+          "Search for services"
+        )}
+        onChange={(event) => onChange(event.target.value)}
+      />
+    </StyledField>
+  );
+};

src/modules/service-catalog/useServiceCatalogItems.spec.ts
@@ -1,0 +1,142 @@
+import { renderHook, act } from "@testing-library/react-hooks";
+import { useServiceCatalogItems } from "./useServiceCatalogItems";
+
+describe("useServiceCatalogItems", () => {
+  const mockResponse = {
+    service_catalog_items: [
+      { id: 1, name: "Item 1", description: "Description 1", form_id: 1 },
+      { id: 2, name: "Item 2", description: "Description 2", form_id: 2 },
+    ],
+    meta: { before_cursor: null, after_cursor: null, has_more: false },
+    count: 2,
+  };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should fetch service catalog items without input", async () => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(mockResponse),
+        status: 200,
+        ok: true,
+      })
+    ) as jest.Mock;
+
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useServiceCatalogItems()
+    );
+
+    act(() => {
+      result.current.fetchServiceCatalogItems("", null);
+    });
+
+    expect(result.current.serviceCatalogItems).toEqual([]);
+    expect(result.current.isLoading).toBe(true);
+
+    await waitForNextUpdate();
+
+    expect(result.current.serviceCatalogItems).toEqual(
+      mockResponse.service_catalog_items
+    );
+    expect(result.current.isLoading).toBe(false);
+    expect(result.current.meta).toEqual(mockResponse.meta);
+    expect(result.current.count).toEqual(mockResponse.count);
+  });
+
+  it("should handle fetch error", async () => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve({ message: "Error fetching" }),
+        status: 500,
+        ok: false,
+      })
+    ) as jest.Mock;
+
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useServiceCatalogItems()
+    );
+
+    act(() => {
+      result.current.fetchServiceCatalogItems("", null);
+    });
+
+    expect(result.current.serviceCatalogItems).toEqual([]);
+    expect(result.current.isLoading).toBe(true);
+
+    await waitForNextUpdate();
+
+    expect(result.current.serviceCatalogItems).toEqual([]);
+    expect(result.current.isLoading).toBe(false);
+    expect(result.current.errorFetchingItems).toEqual(
+      new Error("HTTP error! status: 500")
+    );
+  });
+
+  it("should refetch items when fetchServiceCatalogItems is called", async () => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(mockResponse),
+        status: 200,
+        ok: true,
+      })
+    ) as jest.Mock;
+
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useServiceCatalogItems()
+    );
+
+    act(() => {
+      result.current.fetchServiceCatalogItems("", null);
+    });
+
+    expect(result.current.serviceCatalogItems).toEqual([]);
+    expect(result.current.isLoading).toBe(true);
+
+    await waitForNextUpdate();
+
+    expect(result.current.serviceCatalogItems).toEqual(
+      mockResponse.service_catalog_items
+    );
+    expect(result.current.isLoading).toBe(false);
+    expect(result.current.meta).toEqual(mockResponse.meta);
+    expect(result.current.count).toEqual(mockResponse.count);
+
+    const newMockResponse = {
+      service_catalog_items: [
+        { id: 3, name: "Item 3", description: "Description 3", form_id: 3 },
+      ],
+      meta: { before_cursor: null, after_cursor: null, has_more: false },
+      count: 3,
+    };
+
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(newMockResponse),
+        status: 200,
+        ok: true,
+      })
+    ) as jest.Mock;
+
+    act(() => {
+      result.current.fetchServiceCatalogItems("Item 3", null);
+    });
+
+    expect(result.current.serviceCatalogItems).toEqual(
+      mockResponse.service_catalog_items
+    );
+    expect(result.current.isLoading).toBe(true);
+
+    await waitForNextUpdate();
+
+    expect(result.current.serviceCatalogItems).toEqual(
+      newMockResponse.service_catalog_items
+    );
+
+    expect(result.current.isLoading).toBe(false);
+
+    expect(result.current.meta).toEqual(newMockResponse.meta);
+    expect(result.current.count).toEqual(newMockResponse.count);
+  });
+});