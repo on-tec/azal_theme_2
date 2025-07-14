zendesk/copenhagen_theme

feat: adds support for multiple dynamic filters

src/modules/new-request-form/fields/LookupField.test.tsx
@@ -1,7 +1,8 @@
 import type { Field, LookupRelationshipFieldFilter } from "../data-types";
 import { buildAdvancedDynamicFilterParams } from "./LookupField";
+
 describe("buildAdvancedDynamicFilterParams", () => {
-  it('returns correct dynamic filter value and matching field value for operator "matches"', () => {
+  it('returns correct dynamic filter array for operator "matches"', () => {
     const filter: LookupRelationshipFieldFilter = {
       all: [
         {
@@ -38,16 +39,12 @@ describe("buildAdvancedDynamicFilterParams", () => {
       },
     ];
 
-    const [dynamicValue, fieldValue] = buildAdvancedDynamicFilterParams(
-      filter,
-      fields
-    );
+    const result = buildAdvancedDynamicFilterParams(filter, fields);
 
-    expect(dynamicValue).toBe("ticket_fields_12345");
-    expect(fieldValue).toBe("fooValue");
+    expect(result).toEqual([{ key: "ticket_fields_12345", value: "fooValue" }]);
   });
 
-  it('returns correct dynamic filter value and matching field value for operator "not_matches" in any', () => {
+  it('returns correct dynamic filter array for operator "not_matches" in any', () => {
     const filter: LookupRelationshipFieldFilter = {
       all: [{ field: "someField", operator: "is", value: "foo" }],
       any: [
@@ -83,16 +80,12 @@ describe("buildAdvancedDynamicFilterParams", () => {
       },
     ];
 
-    const [dynamicValue, fieldValue] = buildAdvancedDynamicFilterParams(
-      filter,
-      fields
-    );
+    const result = buildAdvancedDynamicFilterParams(filter, fields);
 
-    expect(dynamicValue).toBe("ticket_fields_67890");
-    expect(fieldValue).toBe("barValue");
+    expect(result).toEqual([{ key: "ticket_fields_67890", value: "barValue" }]);
   });
 
-  it("returns [undefined, undefined] when no matching operator found", () => {
+  it("returns an empty array when no matching operator found", () => {
     const filter: LookupRelationshipFieldFilter = {
       all: [{ field: "someField", operator: "is", value: "foo" }],
       any: [{ field: "anotherField", operator: "is", value: "foo" }],
@@ -122,16 +115,12 @@ describe("buildAdvancedDynamicFilterParams", () => {
       },
     ];
 
-    const [dynamicValue, fieldValue] = buildAdvancedDynamicFilterParams(
-      filter,
-      fields
-    );
+    const result = buildAdvancedDynamicFilterParams(filter, fields);
 
-    expect(dynamicValue).toBeUndefined();
-    expect(fieldValue).toBeUndefined();
+    expect(result).toEqual([]);
   });
 
-  it("returns [undefined, undefined] when filter is undefined", () => {
+  it("returns an empty array when filter is undefined", () => {
     const fields: Field[] = [
       {
         id: 12345,
@@ -146,12 +135,57 @@ describe("buildAdvancedDynamicFilterParams", () => {
       },
     ];
 
-    const [dynamicValue, fieldValue] = buildAdvancedDynamicFilterParams(
-      undefined,
-      fields
-    );
+    const result = buildAdvancedDynamicFilterParams(undefined, fields);
 
-    expect(dynamicValue).toBeUndefined();
-    expect(fieldValue).toBeUndefined();
+    expect(result).toEqual([]);
+  });
+
+  it("returns multiple filters if more than one matches", () => {
+    const filter: LookupRelationshipFieldFilter = {
+      all: [
+        {
+          field: "someField",
+          operator: "matches",
+          value: "ticket_fields_12345",
+        },
+        {
+          field: "otherField",
+          operator: "matches",
+          value: "ticket_fields_67890",
+        },
+      ],
+      any: [],
+    };
+    const fields: Field[] = [
+      {
+        id: 12345,
+        name: "Test Field 1",
+        value: "fooValue",
+        error: null,
+        label: "Test Field 1",
+        required: false,
+        description: "",
+        type: "text",
+        options: [],
+      },
+      {
+        id: 67890,
+        name: "Test Field 2",
+        value: "barValue",
+        error: null,
+        label: "Test Field 2",
+        required: false,
+        description: "",
+        type: "text",
+        options: [],
+      },
+    ];
+
+    const result = buildAdvancedDynamicFilterParams(filter, fields);
+
+    expect(result).toEqual([
+      { key: "ticket_fields_12345", value: "fooValue" },
+      { key: "ticket_fields_67890", value: "barValue" },
+    ]);
   });
 });

on-tec/azal_theme_2

src/modules/ticket-fields/fields/LookupField.test.tsx
@@ -3,7 +3,7 @@ import type { TicketFieldObject } from "../data-types/TicketFieldObject";
 import { buildAdvancedDynamicFilterParams } from "./LookupField";
 
 describe("buildAdvancedDynamicFilterParams", () => {
-  it('returns correct dynamic filter value and matching field value for operator "matches"', () => {
+  it('returns correct dynamic filter array for operator "matches"', () => {
     const filter: LookupRelationshipFieldFilter = {
       all: [
         {
@@ -40,16 +40,12 @@ describe("buildAdvancedDynamicFilterParams", () => {
       },
     ];
 
-    const [dynamicValue, fieldValue] = buildAdvancedDynamicFilterParams(
-      filter,
-      fields
-    );
+    const result = buildAdvancedDynamicFilterParams(filter, fields);
 
-    expect(dynamicValue).toBe("ticket_fields_12345");
-    expect(fieldValue).toBe("fooValue");
+    expect(result).toEqual([{ key: "ticket_fields_12345", value: "fooValue" }]);
   });
 
-  it('returns correct dynamic filter value and matching field value for operator "not_matches" in any', () => {
+  it('returns correct dynamic filter array for operator "not_matches" in any', () => {
     const filter: LookupRelationshipFieldFilter = {
       all: [{ field: "someField", operator: "is", value: "foo" }],
       any: [
@@ -85,16 +81,12 @@ describe("buildAdvancedDynamicFilterParams", () => {
       },
     ];
 
-    const [dynamicValue, fieldValue] = buildAdvancedDynamicFilterParams(
-      filter,
-      fields
-    );
+    const result = buildAdvancedDynamicFilterParams(filter, fields);
 
-    expect(dynamicValue).toBe("ticket_fields_67890");
-    expect(fieldValue).toBe("barValue");
+    expect(result).toEqual([{ key: "ticket_fields_67890", value: "barValue" }]);
   });
 
-  it("returns [undefined, undefined] when no matching operator found", () => {
+  it("returns an empty array when no matching operator found", () => {
     const filter: LookupRelationshipFieldFilter = {
       all: [{ field: "someField", operator: "is", value: "foo" }],
       any: [{ field: "anotherField", operator: "is", value: "foo" }],
@@ -124,13 +116,9 @@ describe("buildAdvancedDynamicFilterParams", () => {
       },
     ];
 
-    const [dynamicValue, fieldValue] = buildAdvancedDynamicFilterParams(
-      filter,
-      fields
-    );
+    const result = buildAdvancedDynamicFilterParams(filter, fields);
 
-    expect(dynamicValue).toBeUndefined();
-    expect(fieldValue).toBeUndefined();
+    expect(result).toEqual([]);
   });
 
   it("returns [undefined, undefined] when filter is undefined", () => {
@@ -148,12 +136,57 @@ describe("buildAdvancedDynamicFilterParams", () => {
       },
     ];
 
-    const [dynamicValue, fieldValue] = buildAdvancedDynamicFilterParams(
-      undefined,
-      fields
-    );
+    const result = buildAdvancedDynamicFilterParams(undefined, fields);
 
-    expect(dynamicValue).toBeUndefined();
-    expect(fieldValue).toBeUndefined();
+    expect(result).toEqual([]);
+  });
+
+  it("returns multiple filters if more than one matches", () => {
+    const filter: LookupRelationshipFieldFilter = {
+      all: [
+        {
+          field: "someField",
+          operator: "matches",
+          value: "ticket_fields_12345",
+        },
+        {
+          field: "otherField",
+          operator: "matches",
+          value: "ticket_fields_67890",
+        },
+      ],
+      any: [],
+    };
+    const fields: TicketFieldObject[] = [
+      {
+        id: 12345,
+        name: "Test Field 1",
+        value: "fooValue",
+        error: null,
+        label: "Test Field 1",
+        required: false,
+        description: "",
+        type: "text",
+        options: [],
+      },
+      {
+        id: 67890,
+        name: "Test Field 2",
+        value: "barValue",
+        error: null,
+        label: "Test Field 2",
+        required: false,
+        description: "",
+        type: "text",
+        options: [],
+      },
+    ];
+
+    const result = buildAdvancedDynamicFilterParams(filter, fields);
+
+    expect(result).toEqual([
+      { key: "ticket_fields_12345", value: "fooValue" },
+      { key: "ticket_fields_67890", value: "barValue" },
+    ]);
   });
 });